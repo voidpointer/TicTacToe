664c59f732d99dfb7a3d788e36097aeb5c0410cc
Ryan Jones <voidpointer@hotmail.com>
2015-04-16 12:52:47 -0500

    Added logic to play tic tac toe and determine a winner.  (only lets you play one game and will tell you who won.  you need to restart the app if you want to play another game.  does not do anything to check for a cat's game)

--- TicTacToe/ViewController.swift
+++ TicTacToe/ViewController.swift
@@ -30,7 +30,7 @@ public enum SquareMark {
     case BLANK
 }
 
-class GameViewController: UIViewController {
+class GameViewController: UIViewController, TicTacToeProtocol {
 
     @IBOutlet weak var square1: UIButton!
     @IBOutlet weak var square2: UIButton!
@@ -44,12 +44,101 @@ class GameViewController: UIViewController {
     
     @IBOutlet var gameSquares: [UIButton]!
     
+    // turn keeps track of which turn it is and alternates between .X and .O
+    var turn = SquareMark.X;
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // blank out the tiles at the beginning of the game
+        for square in gameSquares {
+            square.setTitle("", forState:UIControlState.Normal);
+        }
     }
 
-    //TODO implement protocol
+    @IBAction func squareTapped(sender: UIButton) {
+        var row = sender.tag / 3,
+            column = sender.tag % 3,
+            square = TicTacToeSquare(row:row, column:column);
+        
+        // check to see if this square has already been played
+        if (GetSquareMark(square) != .BLANK) {
+            return;
+        }
+        
+        SetSquareMark(square, mark:turn);
+        
+        if (IsWinningLinePresent()) {
+            // pop up a winning message
+            var refreshAlert = UIAlertController(title: "Game Over", message: (turn == .X ? "X" : "O") + " wins!", preferredStyle: UIAlertControllerStyle.Alert)
+            presentViewController(refreshAlert, animated: true, completion: nil)
+        }
+        
+        // alternate turns
+        turn = turn == .X ? .O : .X;
+    }
+    
+    /*
+    CheckWinningLine is used by IsWinningLinePresent to see if three squares 
+    have the same value (and that value is not BLANK)
+    Note: We don't have to check if they're X's or O's, we just know that the last
+    player to make a move is the winner.
+    */
+    func CheckWinningLine(row1: Int, column1: Int, row2: Int, column2: Int, row3: Int, column3: Int) -> Bool {
+        var mark1 = GetSquareMark(TicTacToeSquare(row: row1, column: column1));
+        var mark2 = GetSquareMark(TicTacToeSquare(row: row2, column: column2));
+        var mark3 = GetSquareMark(TicTacToeSquare(row: row3, column: column3));
+        
+        if (mark1 != .BLANK &&
+            mark1 == mark2 &&
+            mark2 == mark3) {
+            return true;
+        }
+        return false;
+    }
 
+    func GetSquareMark(square: TicTacToeSquare) -> SquareMark {
+        var squareIndex = square.row * 3 + square.column;
+        
+        if (gameSquares[squareIndex].titleLabel?.text == "X") {
+            return .X;
+        }
+        else if (gameSquares[squareIndex].titleLabel?.text == "O") {
+            return .O;
+        }
+        else {
+            return .BLANK;
+        }
+    }
+    
+    func SetSquareMark(square: TicTacToeSquare, mark: SquareMark) -> Void {
+        var squareIndex = square.row * 3 + square.column;
+        gameSquares[squareIndex].setTitle(mark == .X ? "X" : "O", forState:UIControlState.Normal);
+    }
+    
+    func IsWinningLinePresent() -> Bool {
+        // horizontal lines
+        if (CheckWinningLine(0, column1: 0, row2: 0, column2: 1, row3: 0, column3: 2) ||
+            CheckWinningLine(1, column1: 0, row2: 1, column2: 1, row3: 1, column3: 2) ||
+            CheckWinningLine(2, column1: 0, row2: 2, column2: 1, row3: 2, column3: 2)) {
+            return true;
+        }
+        
+        // vertical lines
+        if (CheckWinningLine(0, column1: 0, row2: 1, column2: 0, row3: 2, column3: 0) ||
+            CheckWinningLine(0, column1: 1, row2: 1, column2: 1, row3: 2, column3: 1) ||
+            CheckWinningLine(0, column1: 2, row2: 1, column2: 2, row3: 2, column3: 2)) {
+            return true;
+        }
+        
+        // diagonal lines
+        if (CheckWinningLine(0, column1: 0, row2: 1, column2: 1, row3: 2, column3: 2) ||
+            CheckWinningLine(2, column1: 0, row2: 1, column2: 1, row3: 0, column3: 2)) {
+            return true;
+        }
+        
+        return false;
+    }
 }
 
